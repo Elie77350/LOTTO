843e0c444e9db5797bc5d3cbaa3a777c2a8daa54
Elie77350 <75457081+Elie77350@users.noreply.github.com>
2022-02-25 22:58:54 +0100

    Initial Commit

---
+++ LOTTO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LOTTO
+//
+//  Created by Élie BINGUE on 25/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
